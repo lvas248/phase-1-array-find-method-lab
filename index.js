e9a19e7b3dd52750831f4efe0121036fd71ad0d9
Luis <lvas248@gmail.com>
2022-05-24 14:16:19 -0400

    commit tree 26460e59fc66ff75114ca7f3716509b835dd749c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(array){
+      if(array.find(func)){
+          return array.find(func).year;
+      }
+    }
+  
+  
+  function func(item, index, array){
+      return item.result === "W";}
